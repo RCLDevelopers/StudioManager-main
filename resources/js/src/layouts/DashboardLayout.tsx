cfcdd1213b8930e74d358a9117a64d06d5267691
RCLDevelopers <rasshtech@gmail.com>
2025-04-04 15:02:48 +0300

    first commit

--- resources/js/src/layouts/DashboardLayout.tsx
+++ resources/js/src/layouts/DashboardLayout.tsx
@@ -1,11 +1,12 @@
+import React from 'react';
 import { Box } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 
 const DashboardLayout = () => {
   return (
-    <Box sx={{ display: 'flex', minHeight: '100vh' }}>
+    <Box sx={{ display: 'flex', minHeight: '100vh', bgcolor: 'background.default' }}>
       {/* Add your Sidebar component here */}
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+      <Box component="main" sx={{ flexGrow: 1, p: 0 }}>
         <Outlet />
       </Box>
     </Box>
